f31b24676bf04e395ed55e002ad5a3728d212734
nazmulsisil <sisil8sisil@gmail.com>
2024-06-15 18:50:06 +0600

    fix: Folders

--- src/pages/api/scheduled-check.js
+++ src/pages/api/scheduled-check.js
@@ -2,8 +2,8 @@ import checkUrls from "./check-urls";
 
 export default async (req, res) => {
   try {
-    await checkUrls(req, res);
-    res.status(200).send("Scheduled URL check executed.");
+    const result = await checkUrls();
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send("Failed to execute scheduled URL check.");
   }
